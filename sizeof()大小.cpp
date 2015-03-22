4826ccb6ec1f39702813c176ee2bbec55db7e527
Modiz <504281906@qq.com>
2015-03-22 20:26:12 +0800

    关于sizeof存储大小的分配
    VC对变量存储的一个特殊处理。为了提高CPU的存储速度，VC对一些变量的起始地址做了“对齐”处理。在默认情况下，VC规定各成员变量存放的起始地址相对于结构的起始地址的偏移量必须为该变量的类型所占用的字节数的倍数。

---
+++ sizeof()大小.cpp
@@ -0,0 +1,45 @@
+//http://www.jb51.net/article/36020.htm 
+#include<limits>
+#include<queue>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+
+#define LL __int64
+#define eps 1e-8
+#define pi acos(-1)
+#define INF 0x7fffffff
+#define delta 0.98 //模拟退火递增变量
+using namespace std;
+int a;
+char b;
+#pragma pack(2) 
+struct A{
+	char c;
+	double a;
+	int b;
+	//char c;
+	//char d;
+}; 
+int main(){
+	cout<<sizeof(a)<<endl;
+	cout<<sizeof(b)<<endl;
+	cout<<sizeof(A)<<endl;
+	return 0;
+}
